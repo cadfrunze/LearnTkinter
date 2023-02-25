dbff4cc319bb5fa5a742ae675034a42dba6511a2
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2023-02-25 23:41:47 +0200

    new

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-with open('work_log.txt', 'a') as file:
-    file.writelines('25.02.2023')
+# def add(*args):
+#     sum = 0
+#     for n in args:
+#         sum = sum + n
+#     return sum
+
+
+def calculate(n, **kwargs):
+    print(f"{n}  + {kwargs['add']} = ", n + kwargs['add'])
+    print(f"{n} * {kwargs['multiply']} = ", n * kwargs['multiply'])
+
+
+calculate(n=2, add=5, multiply=4)
